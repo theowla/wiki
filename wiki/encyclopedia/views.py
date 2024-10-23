44c9784d8ead3d0ba90eb36d9f70880c6f51250c
Theo Wlasiczuk Ballesteros <124530613+theowla@users.noreply.github.com>
2024-10-23 19:58:11 +0000

    some changes in urls and html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
-
+def convertToHtml(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+    
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-    
 
-
+def entry(request, title):
+    html_content = convertToHtml(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html")
